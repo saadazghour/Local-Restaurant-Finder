5e3ca8ae0be6226a8baf3c7e6c73a356b498a07a
saadazghour <saad.azghour@gmail.com>
2023-12-12 01:22:14 +0100

    refactor: Update navigation structure in App
    - Remove unnecessary imports and styling
    - Refactor the App component to use the NavigationContainer directly
    - Simplify the rendering of RestaurantList and RestaurantDetails screens in the Stack.Navigator
    - Remove unused Header component import
    - Clean up and improve code readability

--- App.tsx
+++ App.tsx
@@ -3,29 +3,34 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import RestaurantList from "./src/components/HomeScreen";
 import RestaurantDetails from "./src/components/RestaurantDetails";
+import Header from "./src/components/Header";
+// import "./src/App.css";
+import { View } from "react-native";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      {/*
+    <>
+      <NavigationContainer>
+        {/*
       
       RestaurantList and RestaurantDetails are components rendered by Stack.Navigator, they will receive the navigation prop. You can use this prop to navigate between screens.
       
-      */}
+    */}
 
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen
-          name="Home"
-          options={{ title: "Restaurants" }}
-          component={RestaurantList}
-        />
-        <Stack.Screen
-          name="Restaurants Details"
-          component={RestaurantDetails}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen
+            name="Home"
+            options={{ title: "Restaurants" }}
+            component={RestaurantList}
+          />
+          <Stack.Screen
+            name="Restaurants Details"
+            component={RestaurantDetails}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   );
 }
